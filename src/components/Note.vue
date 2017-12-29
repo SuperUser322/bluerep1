1ba34490ff5c3d47ba0566b5ba353709e7fa45a1
Alex <borntorunthecopsdonotcatch@gmail.com>
2017-12-29 22:57:13 +0200

    add ability to edit notes

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
     <div>
-      <input v-model.trim='newNote' type='text'/>
+      <input v-model.trim='newNote.text' type='text'/>
       <button @click="createNote">Add</button>
     </div>
     <div v-for='note in notes'>
-      <input type='checkbox' />
-      <span>{{note}}</span>
-      <button>delete</button>
+      <div v-if='note.editMode'>
+        <input v-model='note.text' type='text' />
+        <button @click='saveNote(note)'>Save</button>
+      </div>
+      <div v-else>
+        <input v-model='note.completed' type='checkbox' />
+        <span :class="{ completed: note.completed }">
+          {{note.text}}
+        </span>
+        <button @click="changeNote(note)">Edit</button>
+        <button @click="removeNote(note)">delete</button>
+      </div>
     </div>
   </div>
 </template>
@@ -15,16 +24,35 @@
 <script>
   export default {
     data: () => ({
-      newNote: '',
+      newNote: {
+        text: '',
+        completed: false,
+        editMode: false,
+      },
       notes: []
     }),
     methods: {
       createNote () {
-        if (this.newNote !== "") {
+        if (this.newNote.text !== '') {
           this.notes.push(this.newNote)
-          this.newNote = ''
+          this.newNote = { text: '', completed: false, editMode: false }
         }
+      },
+      removeNote (note) {
+        this.notes.splice(this.notes.findIndex((el) => el === note), 1)
+      },
+      changeNote (note) {
+        note.editMode = true
+      },
+      saveNote (note) {
+        note.editMode = false
       }
     }
   }
 </script>
+
+<style>
+.completed {
+  text-decoration: line-through;
+}
+</style>
