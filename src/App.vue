30aec12e0c9c5bf2c901693e0b3d659240c92f68
Alex <borntorunthecopsdonotcatch@gmail.com>
2017-12-29 21:56:35 +0200

    add ability to create notes

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Note />
+  </div>
+</template>
+
+<script>
+import Note from './components/Note.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Note
+  }
+}
+</script>
---
+++ src/components/Note.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <input v-model.trim='newNote' type='text'/>
+      <button @click="createNote">Add</button>
+    </div>
+    <div v-for='note in notes'>
+      <input type='checkbox' />
+      <span>{{note}}</span>
+      <button>delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      newNote: '',
+      notes: []
+    }),
+    methods: {
+      createNote () {
+        if (this.newNote !== "") {
+          this.notes.push(this.newNote)
+          this.newNote = ''
+        }
+      }
+    }
+  }
+</script>
